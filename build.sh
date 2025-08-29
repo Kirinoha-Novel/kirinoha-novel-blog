bee2b90d4decbe68c05eb43c62cdd464610a6205
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-08-29 22:29:11 +0900

    chore: add shell script

---
+++ build.sh
@@ -0,0 +1,15 @@
+# Reference: https://pablomarti.dev/deploy-zola-to-clouflare-workers/
+#!/bin/sh
+
+ZOLA_VERSION=0.21.0
+
+# Install Zola
+echo "Installing Zola ${ZOLA_VERSION}..."
+curl -sLJO "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v{$ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+mkdir "${HOME}/.local/zola"
+tar -C "${HOME}/.local/zola" -xf "zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+rm "zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+export PATH="${HOME}/.local/zola:${PATH}"
+
+# Build blog
+zola build --minify
